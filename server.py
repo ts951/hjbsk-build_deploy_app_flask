13ed1f92f224bda6132c6e85bee3b5c0af7c8603
Thomas Squires <thomassquires951@gmail.com>
2025-09-13 15:43:53 +0100

    use functions created in maths package for the sum, sub and mul endpoints

--- server.py
+++ server.py
@@ -1,29 +1,53 @@
 from flask import Flask, render_template, request
-# Import the Maths package here
+from maths.mathematics import summation, subtraction, multiplication
 
 app = Flask("Mathematics Problem Solver")
 
 @app.route("/sum")
 def sum_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    # Write your code here
+    num1 = float(request.args.get("num1"))
+    num2 = float(request.args.get("num2"))
+    result = summation(num1, num2)
+
+    if result == None:
+        return "Invalid input"
+    
+    elif result.is_integer():
+        result = int(result)
+
+    return str(result)        
 
 @app.route("/sub")
 def sub_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    # Write your code here
+    num1 = float(request.args.get("num1"))
+    num2 = float(request.args.get("num2"))
+    result = subtraction(num1, num2)
+
+    if result == None:
+        return "Invalid input"
+
+    if result.is_integer():
+        result = int(result)
+
+    return str(result)  
 
 @app.route("/mul")
 def mul_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    # Write your code here  
+    num1 = float(request.args.get("num1"))
+    num2 = float(request.args.get("num2"))
+    result = multiplication(num1, num2)
+
+    if result == None:
+        return "Invalid input"
+
+    if result.is_integer():
+        result = int(result)
+
+    return str(result)  
 
 @app.route("/")
 def render_index_page():
-    # Write your code here
+    return render_template("index.html")
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
